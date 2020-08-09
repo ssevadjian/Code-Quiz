f5f77272a5b9b62db798a2d9901237a860385373
SJS <sjsevadjian@gmail.com>
2020-08-08 23:51:48 -0700

    tried fixing input form & tried fixing loop of answers

--- questions.js
+++ questions.js
@@ -2,51 +2,51 @@ let questionAndAnswers = [
     {
         q: `1. What is the object called that lets you work with both dates and time-related data?`,
         choices: ["Dates", "Time-warp", "Time field", "Clock"],
-        answer: "Dates"
+        answerKey: "Dates"
     },
     {
         q: `2. What can loops offer JavaScript code as a whole?`,
         choices: ["Add plug-ins", "Improved performance", "Cleaner syntax", "Cross-platform support"],
-        answer: "Improved performance"
+        answerKey: "Improved performance"
     },
     {
-        q: "3. What is the name of the statement that is used to exit or end a loop?",
+        q: `3. What is the name of the statement that is used to exit or end a loop?`,
         choices: ["Close statement", "Conditional statement", "Falter statement", "Break statement"],
-        answer: "Break statement"
+        answerKey: "Break statement"
     },
     {
         q: `4. In JavaScript, what is used in conjunction with HTML to “react” to certain elements?`,
         choices: ["Events", "RegExp", "Condition", "Boolean"],
-        answer: "Events"
+        answerKey: "Events"
     },
     {
         q: `5. In JavaScript, what element is used to store and manipulate text, usually in multiples?`,
         choices: ["Variable", "Recorders", "Arrays", "Strings"],
-        answer: "Strings"
+        answerKey: "Strings"
     },
     {
         q: `6. What is the language or list of instructions that are executed by the computer (how JavaScript is built)?`,
         choices: ["Syntax", "JSON", "Scope", "Output"],
-        answer: "Syntax"
+        answerKey: "Syntax"
     },
     {
         q: `7. What is considered to be the most popular programming language in the world?`,
         choices: ["Swift", "HTML", "JavaScript", "Ruby"],
-        answer: "JavaScript"
+        answerKey: "JavaScript"
     },
     {
-        q: "8. This is what you call the guide that defines coding conventions for all projects.",
+        q: `8. This is what you call the guide that defines coding conventions for all projects.`,
         choices: ["Style guide", "Main textbook", "Coding dictionary", "Developer's Reference"],
-        answer: "Developer's Reference"
+        answerKey: "Developer's Reference"
     },
     {
         q: "9. What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
         choices: ["While Loop", "Else Loop", "For Loop", "Conditional Loop"],
-        answer: "While Loop"
+        answerKey: "While Loop"
     },
     {
         q: "10. Where is the JavaScript placed inside an HTML document or page?",
         choices: ["In the <body> and <head> sections.", "In the <meta> section.", "In the <title> section.", "In the <footer> section."],
-        answer: "In the <body> and <head> sections."
+        answerKey: "In the <body> and <head> sections."
     }
 ];
